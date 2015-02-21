4356c8e2e199cd0bc78b77c1ee47a22a0b47b290
Jacob Willford <jawillford@fullsail.edu>
2015-02-20 20:11:02 -0600

    Finished the duel program

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -1,77 +1,99 @@
-/**
- * Jacob Willford - goal1_duel1
- * Date: 02/07
-*/
+//Jacob Willford 02/20/15
 
-// self-executing function
+//self executing function
 (function(){
 
-    console.log("FIGHT!");
+    //set the round number for the fight
+    var round = 1;
 
-    //player name
-    playerName = ["Superman","Goku"];
+    // create and object for all the fighters
+    var fighters=[{name:"Goku",damage:20,health:100}, {name:"Vegeta",damage:20,health:100}];
 
-    //player damage
-    playerDamage = ["20","20"];
+    //grab the tag
+    var fighter1text = document.querySelector("#kabal").querySelector("p");
 
-    //player health
-    playerHealth = ["100","100"];
+    //grab the tag
+    var fighter2text = document.querySelector("#kratos").querySelector("p");
 
-    //start round
-    var round=0;
+    //grab the area for the round text
+    var roundText = document.getElementById("round");
 
+    //grab the fight button
+    var button = document.getElementById("fight_btn");
+
+    //sets the round text
+    roundText.innerHTML=fighters[0].name+" VS "+fighters[1].name;
+
+    //when the button is clicked it runs the fight function
+    button.addEventListener("click", fight, false);
+
+    //creates the fight function
     function fight(){
-        alert(playerName[0]+":"+playerHealth[0]+"  *START*  "+playerName[1]+":"+playerHealth[1]);
-        for (var i = 0; i < 10; i++)
-        {
-            //formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = playerDamage[0] * .5;
-            var minDamage2 = playerDamage[1] * .5;
-            var f1 = Math.floor(Math.random()*(playerDamage[0]-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(playerDamage[1]-minDamage2)+minDamage2);
 
-            //inflict damage
-            playerHealth[0]-=f1;
-            playerHealth[1]-=f2;
+        //use innerHTML to set the round text
+        roundText.innerHTML="Round "+round+" Over!";
 
-            console.log(playerName[0]+": "+playerHealth[0] + " " + playerName[1]+":"+playerHealth[1]);
+        //create minimum damage variables
+        var minDamage1 = fighters[0].damage * .5;
+        var minDamage2 = fighters[1].damage * .5;
 
-            var result = winnerCheck();
-            //display result in console
-            console.log(result);
-            //if result is equal to "no winner" then increment round and alert this
-            if (result==="no winner")
-            {
-                round++;
-                alert(playerName[0]+":"+playerHealth[0]+"  *ROUND "+round+" OVER"+"*  "+playerName[1]+":"+playerHealth[1]);
-//else alert result
-            } else{
-                alert(result);
-                break;
-            }
+        //choose random damage
+        var f1 = Math.floor(Math.random()*(fighters[0].damage-minDamage1) + minDamage1);
+        var f2 = Math.floor(Math.random()*(fighters[1].damage-minDamage2) + minDamage2);
 
-          }
-    }
-//function winnercheck
-    function winnerCheck(){
-        var result="no winner";
-        //if playeronehealth is less than 1 and player two health is less than 1 display "They both have died"
-        if (playerHealth[0]<1 && playerHealth[1]<1)
-        {
-            result = "They have both died";
-        //else if playeronehealth is less than 1 display "playertwoname + 'WINS!'"
-        } else if(playerHealth[0]<1){
-            result =playerName[1]+" WINS!";
-        //else if playertwohealth is less than 1 display "playertwoname + 'WINS!'"
-        } else if (playerHealth[1]<1)
-        {
-            result = playerName[0]+" WINS!";
+        //inflict damage
+        fighters[0].health -= f1;
+        fighters[1].health -= f2;
+
+        //run winner check function
+        var results = winnerCheck();
+
+        //check if the result is no winner
+        if(results==="no winner"){
+
+            //add to round number
+            round++;
+
+            //show fighter1's name and health
+            fighter1text.innerHTML=fighters[0].name+": "+fighters[0].health;
+
+            //show fighter 2's name and health
+            fighter2text.innerHTML=fighters[1].name+": "+fighters[1].health;
+
+        }else{
+
+            //make the button unable to be clicked
+            button.removeEventListener("click",fight,false);
+
+            //change button text to "DONE!!"
+            document.querySelector(".buttonblue").innerHTML="DONE!!";
         }
-       //return result
-        return result;
     }
+    //creates the winner check function
+    function winnerCheck(){
 
-    //The program begins here
-    fight();
+        //create a result var and set it to "no winner"
+        var result = "no winner";
 
+        //if both players die
+        if(fighters[0].health<1&&fighters[1].health<1){
+            result = "You both die!";
+            fighter1text.innerHTML="You both die";
+            fighter2text.innerHTML="";
+
+            //if player one dies
+        }else if(fighters[0].health<1){
+            result = fighters[0].name+" Dies.. "+fighters[1].name+" Wins!";
+            fighter1text.innerHTML="";
+            fighter2text.innerHTML=fighters[1].name+" you have WON the fight!";
+
+            //if player two dies
+        }else if(fighters[1].health<1){
+            result = fighters[1].name+" Dies.. "+fighters[0].name+" Wins!";
+            fighter1text.innerHTML=fighters[0].name+" you have WON the fight!";
+            fighter2text.innerHTML="";
+        }
+        //send back result
+        return result
+    }
 })();
